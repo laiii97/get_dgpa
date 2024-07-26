488b874fe59394394b008ba38a99a069c400b494
LAI <69414053+laiii97@users.noreply.github.com>
2024-07-26 21:02:06 +0800

    0726
    updated website URL

--- main.py
+++ main.py
@@ -1,60 +1,44 @@
-#從www.dgpa.gov.tw取得特定縣市之停班停課狀態
+# 從www.dgpa.gov.tw取得特定縣市之停班停課狀態
 import requests
 from bs4 import BeautifulSoup as bs
 
-cityList = {'基隆市':'city_Name Keelung City','臺北市':'city_Name Taipei City','新北市':'city_Name New Taipei City','桃園市':'city_Name Taoyuan City','新竹市':'city_Name Hsinchu City','新竹縣':'city_Name Hsinchu County','苗栗縣':'city_Name Miaoli County','臺中市':'city_Name Taichung City','彰化縣':'city_Name Changhua County','雲林縣':'city_Name Yunlin County','南投縣':'city_Name Nantou county','嘉義市':'city_Name Chiayi City','嘉義縣':'city_Name Chiayi County','臺南市':'city_Name Tainan City','高雄市':'city_Name Kaohsiung City','屏東縣':'city_Name Pingtung County','宜蘭縣':'city_Name Yilan County','花蓮縣':'city_Name Hualien County','臺東縣':'city_Name Taitung County','澎湖縣':'city_Name Penghu County','連江縣':'city_Name Lienchiang County','金門縣':'city_Name Kinmen County'}
+cityList = ['基隆市','臺北市','新北市','桃園市','新竹市','新竹縣','苗栗縣','臺中市','彰化縣','雲林縣','南投縣','嘉義市','嘉義縣','臺南市','高雄市','屏東縣','宜蘭縣','花蓮縣','臺東縣','澎湖縣','連江縣','金門縣']
 
-def get_dgpa(targetCity):
-    while True:
-        if targetCity == '台灣':
-            get_dgpa_all()
-            exit(0)
-        if targetCity in cityList:
-            targetCity = cityList[targetCity]
-            break
-        else:
-            if targetCity == '台北市': 
-                targetCity = 'city_Name New Taipei City'
-                break
-            print("找不到該縣市, 請嘗試輸入完整縣市名稱 ex:新竹縣")
-            targetCity = input("輸入欲搜尋的縣市: ")
-    
-    website = requests.request("GET", url='https://www.dgpa.gov.tw/')   #請求對象:www.dgpa.gov.tw
-    website.encoding = 'utf-8'  #防止中文亂碼
-    soup = bs(website.text, 'html.parser')  #website.text為html文字檔案
+def get_dgpa(userInput):
+    targetCity = []
+    # 檢查
+    if userInput == "台北市": # 是否有簡/繁問題並修正清單
+        targetCity.append("臺北市")
+    elif userInput == "tw": # 是否要查詢全部 (cityList)
+        targetCity = cityList
+    elif userInput not in cityList:
+        print("錯誤: 請輸入正確的台灣縣市名")
+    else:
+        targetCity.append(userInput)
+
+    website = requests.request("GET", url='https://www.dgpa.gov.tw/typh/daily/nds.html')   # 請求對象:www.dgpa.gov.tw
+    website.encoding = 'utf-8'  # 防止中文亂碼
+    soup = bs(website.text, 'html.parser')  # website.text為html文字檔案
 
     print(soup.title.text)
-    updateTime = soup.find('h4').text.strip() #<h4>標籤為資料更新日期
+    updateTime = soup.find('h4').text.strip() # <h4>標籤為資料更新日期
     if len(updateTime) == 0:
         print("獲取更新時間失敗")
     else:
-        print(updateTime)
+        print(updateTime[:21])
 
-    table_body = soup.find('tbody', class_='Table_Body')  #找到包含縣市資訊的tbody元素
+    table_body = soup.find('tbody', class_='Table_Body')  # 找到包含縣市資訊的tbody元素
     for trRow in table_body.find_all('tr'):
-        if trRow.find('td', headers=targetCity):
-            cityName = trRow.find('td', headers=cityList[range]).text.strip()
-            cityStatus = trRow.find('td', width='70%').text.strip()
-            print(cityName, cityStatus)
+        try:
+            for i in range(len(targetCity)):
+                if trRow.find('td', headers='city_Name').text.strip() == targetCity[i]:
+                    for tdRow in trRow.find_all('td', headers='StopWorkSchool_Info'):
+                        print(" -", targetCity[i])
+                        cityStatus = ""
+                        for status in tdRow.find_all('font'):
+                            cityStatus += status.text.strip() + "\n"
+                    print("  ", cityStatus)
+        except:
+            pass
 
-def get_dgpa_all():
-    website = requests.request("GET", url='https://www.dgpa.gov.tw/')   #請求對象:www.dgpa.gov.tw
-    website.encoding = 'utf-8'  #防止中文亂碼
-    soup = bs(website.text, 'html.parser')  #website.text為html文字檔案
-
-    print(soup.title.text)
-    updateTime = soup.find('h4').text.strip() #<h4>標籤為資料更新日期
-    if len(updateTime) == 0:
-        print("獲取更新時間失敗")
-    else:
-        print(updateTime)
-
-    for range in cityList:
-        table_body = soup.find('tbody', class_='Table_Body')  #找到包含縣市資訊的tbody元素
-        for trRow in table_body.find_all('tr'):
-            if trRow.find('td', headers=cityList[range]):
-                cityName = trRow.find('td', headers=cityList[range]).text.strip()
-                cityStatus = trRow.find('td', width='70%').text.strip()
-                print(cityName, cityStatus)
-
-get_dgpa(input("輸入欲搜尋的縣市: "))
+get_dgpa(input("輸入欲查詢的縣市:\n"))
